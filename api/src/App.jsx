79753ebbc68d20aa60e63b2a4d5cb5e5981429d4
Tech-Toast118 <yehori291@gmail.com>
2026-06-18 10:12:02 +0200

    Initial commit

---
+++ api/src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  function user(){
+   const [query, setQuery] = useState("");
+   const [books, setBooks] = useState([]);
+   const [loading, setLoading] = useState(false);
+
+   const searchBooks = async () => {
+    if (!query.trim()) return;
+    setLoading(true);
+    try{
+      const res = await fetch(
+        'https://openlibrary.org/search.json?q=${encodeURIComponent(query)}'
+      );
+      const data = await res.json();
+      setBooks(data.docs.slice(0, 20));
+    }
+    finally{
+      setLoading(false);
+    }
+   }
+   
+   
+  }
+  return (
+    <>
+    <div id='main'>
+      <h1>Wyszukiwarka książek</h1>
+      <input type='text' placeholder='Wpisz nazwę' value={query} onChange={(e) => setQuery(e.target.value)} id='input'></input>
+      <button onClick={searchBooks}>Wyszukaj</button>
+
+      {loading && <p>Ładowanie...</p>}
+      {books.map((book) =>(
+        <>
+        <div key={book.key} id='div2'>
+
+        </div>
+        <h3>{book.title}</h3>
+
+        <p>Autor: {book.author_name?.join(',') || "brak"}</p>
+
+        <p>rok: {book.first_publish_year || "brak"}"</p>
+        </>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default App

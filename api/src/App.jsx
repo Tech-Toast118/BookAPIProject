031b5b61800a9cec0fb343938848df2b7dcedec5
Tech-Toast118 <yehori291@gmail.com>
2026-06-19 08:14:09 +0200

    Naprawiona wersja

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,53 +1,61 @@
-import { useEffect, useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  function user(){
-   const [query, setQuery] = useState("");
-   const [books, setBooks] = useState([]);
-   const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-   const searchBooks = async () => {
+  const searchBooks = async () => {
     if (!query.trim()) return;
+
     setLoading(true);
-    try{
+
+    try {
       const res = await fetch(
-        'https://openlibrary.org/search.json?q=${encodeURIComponent(query)}'
+        `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`
       );
+
       const data = await res.json();
       setBooks(data.docs.slice(0, 20));
-    }
-    finally{
+    } catch (error) {
+      console.error("Błąd:", error);
+    } finally {
       setLoading(false);
     }
-   }
-   
-   
-  }
+  };
+
   return (
-    <>
-    <div id='main'>
+    <div id="main">
       <h1>Wyszukiwarka książek</h1>
-      <input type='text' placeholder='Wpisz nazwę' value={query} onChange={(e) => setQuery(e.target.value)} id='input'></input>
+
+      <input
+        id="input"
+        type="text"
+        placeholder="Wpisz nazwę"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
       <button onClick={searchBooks}>Wyszukaj</button>
 
       {loading && <p>Ładowanie...</p>}
-      {books.map((book) =>(
-        <>
-        <div key={book.key} id='div2'>
 
+      {books.map((book) => (
+        <div key={book.key} id="div2">
+          <h3>{book.title}</h3>
+
+          <p>
+            Autor: {book.author_name?.join(", ") || "brak"}
+          </p>
+
+          <p>
+            Rok: {book.first_publish_year || "brak"}
+          </p>
         </div>
-        <h3>{book.title}</h3>
-
-        <p>Autor: {book.author_name?.join(',') || "brak"}</p>
-
-        <p>rok: {book.first_publish_year || "brak"}"</p>
-        </>
       ))}
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
